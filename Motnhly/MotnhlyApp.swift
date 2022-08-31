6ab9faa8d0d38d3d390506ab40772273f68427d0
Aivars Meijers <aivars.meijers@gmail.com>
2022-08-31 12:29:11 +0300

    Initial Commit

---
+++ Motnhly/MotnhlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotnhlyApp.swift
+//  Motnhly
+//
+//  Created by Aivars Meijers on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MotnhlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
